fa6ba40a9c6378f7cabc49c2b2f8d8fe91b7e8e6
Wojciech Dybikowski <dybik08@gmail.com>
2021-02-07 13:31:26 +0100

    finish user authorization

--- src/authentication/authentication.controller.ts
+++ src/authentication/authentication.controller.ts
@@ -3,6 +3,7 @@ import Controller from '../interfaces/controller.interface';
 import validationMiddleware from '../middleware/validation.middleware';
 import CreateUserDto from '../user/user.dto';
 import AuthenticationService from './authentication.service';
+import LogInDto from "./login.dto";
 
 class AuthenticationController implements Controller {
     public path = '/auth';
@@ -15,6 +16,7 @@ class AuthenticationController implements Controller {
 
     private initializeRoutes() {
         this.router.post(`${this.path}/register`, validationMiddleware(CreateUserDto), this.registration);
+        this.router.post(`${this.path}/login`, validationMiddleware(LogInDto), this.logIn);
     }
 
     private registration = async (request: express.Request, response: express.Response, next: express.NextFunction) => {
@@ -26,6 +28,24 @@ class AuthenticationController implements Controller {
             } = await this.authenticationService.register(userData);
             response.setHeader('Set-Cookie', [cookie]);
             response.send(user);
+        } catch (error) {
+            next(error);
+        }
+    }
+
+    private logIn = async (request: express.Request, response: express.Response, next: express.NextFunction) => {
+        console.log('request: ', request.body);
+        const userData: LogInDto = request.body;
+        try {
+            const {
+                token,
+                user,
+            } = await this.authenticationService.login(userData);
+
+            response.send({
+                token,
+                user,
+            });
         } catch (error) {
             next(error);
         }
--- src/authentication/authentication.service.ts
+++ src/authentication/authentication.service.ts
@@ -7,6 +7,8 @@ import TokenData from '../interfaces/tokenData.interface';
 import CreateUserDto from '../user/user.dto';
 import User from '../user/user.entity';
 import {Secret} from "jsonwebtoken";
+import LogInDto from "./login.dto";
+import EmailOrPasswordNotCorrectException from "../exceptions/EmailOrPasswordNotCorrectException";
 
 class AuthenticationService {
     private userRepository = getRepository(User);
@@ -44,6 +46,32 @@ class AuthenticationService {
             expiresIn,
             token: jwt.sign(dataStoredInToken, secret as Secret, { expiresIn }),
         };
+    }
+
+    public async login(userData: LogInDto){
+        console.log('userData: ', userData)
+        // @ts-ignore
+        const user: User = await this.userRepository.findOne({
+            email: userData.email
+        });
+
+        if(user){
+            const hashedPassword = await bcrypt.compare(userData.password, user.password);
+
+            if(hashedPassword){
+                const token = this.createToken(user)
+
+                return {
+                    token,
+                    user,
+                };
+            } else {
+                throw new EmailOrPasswordNotCorrectException()
+            }
+
+        } else {
+            throw new EmailOrPasswordNotCorrectException()
+        }
     }
 }
 
